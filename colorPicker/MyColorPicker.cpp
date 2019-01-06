706fdc71fa26166f731d3bebad42b5d7afbee84b
WilliamGQW <Williamgqw@gmail.com>
2019-01-06 17:50:45 -0600

    Upload different kinds of color pickers
    With different color pickers, the image can be filled with different patterns.

---
+++ colorPicker/MyColorPicker.cpp
@@ -0,0 +1,22 @@
+#include "../cs225/HSLAPixel.h"
+#include "../Point.h"
+
+#include "ColorPicker.h"
+#include "MyColorPicker.h"
+
+using namespace cs225;
+
+/**
+ * Picks the color for pixel (x, y).
+ * Using your own algorithm
+ */
+MyColorPicker::MyColorPicker(double times)
+: hue(1), times(times) { }
+
+HSLAPixel MyColorPicker::getColor(unsigned x, unsigned y) {
+  /* @todo [Part 3] */
+  HSLAPixel pixel(hue, 1, 0.5);
+  hue *= times;
+  if (hue >= 360) { hue = 1; }
+  return pixel;
+}
